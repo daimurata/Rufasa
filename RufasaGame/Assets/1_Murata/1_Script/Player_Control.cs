fdc2ed1ab5e27e556961ff72593f253a4bd76a3b
daimurata <nkc.dai.murata@gmail.com>
2019-07-11 10:26:10 +0900

    途中

---
+++ RufasaGame/Assets/1_Murata/1_Script/Player_Control.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの操作
+/// </summary>
+public class Player_Control : MonoBehaviour
+{
+    
+    public GameObject Player;   //プレイヤー
+    public float Speed, Pl_Mov=1;    //スピード,移動
+    public float[] Restriction_X, Restriction_Y;
+
+    /// <summary>
+    /// 初期データ
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// 常に更新
+    /// </summary>
+    void Update()
+    {
+        //移動処理
+        Move_Control();
+        //移動制限
+        Restriction_Mov();
+    }
+
+    /// <summary>
+    /// 移動管理
+    /// </summary>
+    void Move_Control()
+    {
+        //【プレイヤーのポジション】のコードを短くする
+        Vector2 Position= Player.transform.position;
+
+        //左に移動【Aキー】
+        if (Input.GetKey(KeyCode.A))
+        {
+            //左に移動する
+            Position.x -= Pl_Mov;
+        }
+
+        //右に移動【Dキー】
+        if (Input.GetKey(KeyCode.D))
+        {
+            //右に移動
+            Position.x += Pl_Mov;
+        }
+
+        //上に移動【Wキー】
+        if (Input.GetKey(KeyCode.W))
+        {
+            //上に移動する
+            Position.y += Pl_Mov;
+        }
+
+        //下に移動【Sキー】
+        if (Input.GetKey(KeyCode.S))
+        {
+            //下に移動する
+            Position.y -= Pl_Mov;
+        }
+        //【移動データ】を更新する
+        Player.transform.position =new  Vector2(Position.x,Position.y);
+    }
+
+    /// <summary>
+    /// 移動制限
+    /// </summary>
+    void Restriction_Mov()
+    {
+        //【Xのポジション】のコードを短くする
+        float Pl_X = Player.transform.position.x;
+        //【Yのポジション】のコードを短くする
+        float Pl_y = Player.transform.position.y;
+
+        //左側制御
+        if (Pl_X>=Restriction_X[0])
+        {
+            //値を代入
+            Pl_X = Restriction_X[0];
+        }
+
+        //右側制御
+        if (Pl_X<=Restriction_X[1])
+        {
+            //値を代入
+            Pl_X = Restriction_X[1];
+        }
+
+        //上側制御
+        if (Pl_y>=Restriction_Y[0])
+        {
+            //値を代入
+            Pl_y = Restriction_Y[0];
+        }
+
+        //下側制御
+        if (Pl_y<Restriction_Y[1])
+        {
+            //値を代入
+            Pl_y = Restriction_Y[1];
+        }
+        //【制限データ】を更新
+        Player.transform.position = new Vector2(Pl_X,Pl_y);
+    }
+}
---
+++ RufasaGame/Assets/1_Murata/1_Script/Title_Blink.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// TitleSceneの点滅処理
+/// </summary>
+public class Title_Blink : MonoBehaviour
+{
+    public Text Staert_Text;//点滅する画像
+
+    public bool Chamge = false,Click_Chamge=false;//点滅切替
+
+    public float Alpha,Alpha_Speed,click;//α値とαスピードとクリック
+
+
+    /// <summary>
+    /// 初期データ
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+   /// <summary>
+   /// 常に更新
+   /// </summary>
+    void Update()
+    {
+        //α値の変動
+        Staert_Text.GetComponent<Text>().color = new Color(0, 0, 0, Alpha);
+        //点滅0より小さい時
+        Blink_false();
+        //点滅1より大きいとき
+        Blink_true();
+
+        Click_GO();
+    }
+    /// <summary>
+    /// 0より小さい時
+    /// </summary>
+   　void Blink_false()
+    {
+        //Chamgeがfarceなら
+        if (Chamge==false)
+        {
+            //α値を下げる
+            Alpha -= Alpha_Speed * Time.deltaTime;
+            //α値が0以下なら
+            if (Alpha<=0.0f)
+            {
+                //α値を0にする
+                Alpha = 0.0f;
+                //Chamgeをtrueにする
+                Chamge = true;
+            }
+        }
+    }
+    /// <summary>
+    /// 1より大きい時
+    /// </summary>
+    void Blink_true()
+    {
+        //Chamgeがtrueなら
+        if (Chamge == true)
+        {
+            //α値を上げる
+            Alpha += Alpha_Speed * Time.deltaTime;
+            //α値が1以上なら
+            if (Alpha >= 1.0f)
+            {
+                //α値を1にする
+                Alpha = 1.0f;
+                //Chamgeをtrueにする
+                Chamge = false;
+            }
+        }
+    }
+    /// <summary>
+    /// クリックしてシーン移動
+    /// </summary>
+    public void Scene_GO()
+    {
+        //クリックしたよ
+        Click_Chamge = true;
+    }
+    /// <summary>
+    /// クリックされたら
+    /// </summary>
+    void Click_GO()
+    {
+        //Click_Chamgeがtrueなら
+        if (Click_Chamge == true)
+        {
+            //clickに加算
+            click += Time.deltaTime;
+        }
+        //clickが1以上なら
+        if (click >= 1)
+        {
+            //clickは1
+            click = 1;
+            //ゲームシーンへ移動
+            SceneManager.LoadScene("Game_1");
+        }
+    }
+}
