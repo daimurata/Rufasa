2315a67efc46a1b18094c122079cfbc02f2d4443
daimurata <nkc.dai.murata@gmail.com>
2019-10-24 11:02:37 +0900

    素材を使用、ゲーム画面の完成に挑戦中、システムはまだ

---
+++ RufasaGame/Assets/1_Murata/1_Script/Score_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// スコアのスクリプト
+/// </summary>
+public class Score_Manager : MonoBehaviour
+{
+    public Text Score_Text;     //Textオブジェクト
+    public int Score = 0;       //スコア数字
+    public int Up_Score = 100;  //加算する数値
+
+    //初期
+    void Start()
+    {
+        
+    }
+
+    //更新
+    void Update()
+    {
+        Score_Text.text = "Score:" + Score;
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Score += Up_Score;
+        }
+        
+    }
+}
