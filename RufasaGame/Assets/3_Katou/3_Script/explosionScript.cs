b190ba571502f480a80b566515b29fc27e08a85b
DESKTOP-28F38VP\user <nkc.sousuke.katou@gmail.com>
2020-01-23 09:52:59 +0900

    エフェクト編集

--- RufasaGame/Assets/3_Katou/3_Script/explosionScript.cs
+++ RufasaGame/Assets/3_Katou/3_Script/explosionScript.cs
@@ -7,7 +7,7 @@ public class explosionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3.0f);//インスタンスが生成されて0.5秒後に消す
+        Destroy(gameObject, 3.0f);//インスタンスが生成されて3.0秒後に消す
     }
 
     // Update is called once per frame
